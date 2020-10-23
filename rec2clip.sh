e3bcda4fabc3d98be39cc77465115a7c2c46bceb
Oliver Wendell McLaughlin <36379339+olwmc@users.noreply.github.com>
2020-10-23 00:49:46 -0400

    Update rec2clip.sh

--- rec2clip.sh
+++ rec2clip.sh
@@ -1,4 +1,11 @@
+# Generate UUID
 UUID=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 32 | head -n 1)
+
+# Message
 echo Press Q when finished to quit
-ffmpeg -hide_banner -loglevel panic -f alsa -i pulse /tmp/$UUID-out.mp3
-echo file:///tmp/$UUID-out.mp3 | xclip -t text/uri-list -se c 
+
+# FFMPEG pulse record
+ffmpeg -hide_banner -loglevel panic -f alsa -i pulse /tmp/$UUID.mp3
+
+# Put file uri into clipboard
+echo file:///tmp/$UUID.mp3 | xclip -t text/uri-list -se c 
